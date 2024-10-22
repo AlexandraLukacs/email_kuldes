f2b2e99100ab2cafa030e9a6e8001e0de45cf44b
AlexandraLukacs <lszandika15@gmail.com>
2024-10-22 15:53:06 +0200

    validator

--- app/Models/LibUser.php
+++ app/Models/LibUser.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class LibUser extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable, HasApiTokens;
     
     protected $fillable = [
         'name',
         'email',
         'permission'
     ];
+
+    
+    public function isAdmin()  {
+        return $this->role === 0;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,37 +1,31 @@
 <?php
 
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\CopyController;
-use App\Http\Controllers\LendingController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\LibUserController;
-use App\Models\Lending;
+use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//bárki által elérhető
+Route::post('/register',[RegisteredUserController::class, 'store']);
+Route::post('/login',[AuthenticatedSessionController::class, 'store']);
+Route::apiResource('/users', [LibUserController::class]);
+Route::patch('/update-password/{id}', [LibUserController::class, "updatePassword"]);
 
-Route::get('/lib_users', [LibUserController::class, 'index']);
-Route::get('/lib_user/{id}', [LibUserController::class, 'show']);
-Route::post('/lib_user', [LibUserController::class, 'store']);
-Route::put('/lib_user/{id}', [LibUserController::class, 'update']);
-Route::delete('/lib_user/{id}', [LibUserController::class, 'destroy']);
+//autentikált útvonal
+Route::middleware(['auth:sanctum'])
+->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    // Kijelentkezés útvonal
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+});
+//admin
+Route::middleware(['auth:sanctum',Admin::class])
+->group(function () {
 
-Route::get('books', [BookController::class, 'index']);
-Route::get('/book/{id}', [BookController::class, 'show']);
-Route::post('/book', [BookController::class, 'store']);
-Route::put('book/{id}', [BookController::class, 'update']);
-Route::delete('/book/{id}', [BookController::class, 'destroy']);
+});
+Route::get('/admin/users', [LibUserController::class, 'index']);
 
-Route::get('copies', [CopyController::class, 'index']);
-Route::get('/copy/{id}', [CopyController::class, 'show']);
-Route::post('/copy', [CopyController::class, 'store']);
-Route::put('copy/{id}', [CopyController::class, 'update']);
-Route::delete('/copy/{id}', [CopyController::class, 'destroy']);
-
-Route::get('/lendings', [LendingController::class, 'index']);
-Route::get('/lending/{user_id}/{copy_id}/{start}', [LendingController::class, 'show']);
-Route::post('/lending', [LendingController::class, 'store']);
-Route::patch('/lending/{user_id}/{copy_id}/{start}', [LendingController::class, 'update']);
-Route::delete('/lending/{user_id}/{copy_id}/{start}', [LendingController::class, 'destroy']);
